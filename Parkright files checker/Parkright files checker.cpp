47c8c0f967318b8ce0e16be3d90a991e3970c233
Losevsa <losevsa@inbox.ru>
2022-03-12 12:58:47 +0300

    Создание сделал

--- Parkright files checker/Parkright files checker.cpp
+++ Parkright files checker/Parkright files checker.cpp
@@ -4,8 +4,10 @@
 #include <string>
 
 std::string del(std::string p);
-std::string Parseini(std::string param, std::string t);
-void CreateFolders();
+std::string Parseini(std::string param, std::string temp);
+void CreateFolders(std::string path);
+void CreateFiles(std::string path, std::string PathToMakeTxt);
+void CopyFiles(std::string path);
 
 
 int main()
@@ -15,128 +17,57 @@ int main()
     // загружаем инишник
     std::ifstream inifile("pr_check.ini");
 
-    //если нет файла закрываем всё
+    //если нет файла, то создаем инишник и закрываем прогу 
     if (!inifile.is_open())
     {
         std::cout << "ini файл не найден" << std::endl;
         std::cout << "Файл создан в папке с прогой" << std::endl;
+        inifile.close();
         system("pause");
         return -1;
     }
+
     std::string PRDir = "PRDir";
+    std::string DirToCopy = "DirToCopy";
+    std::string FolderTxt = "FolderTxt";
     std::string temp;
+
+    //парсим инишник и присваиваем всем переменным значения
     while (std::getline(inifile, temp))
-    {
-        PRDir = Parseini(PRDir,temp);
+    {   
+        if (PRDir == "PRDir") 
+        {
+            PRDir = Parseini(PRDir, temp);
+        }
+        if (DirToCopy == "DirToCopy")
+        {
+            DirToCopy = Parseini(DirToCopy, temp);
+        }  
+        if (FolderTxt == "FolderTxt")
+        {
+            FolderTxt = Parseini(FolderTxt, temp);
+        }
     }
     
 
+    //выбираем что надо сделать
     int action;
-    std::cout << "1. Создать список файлов (в ini должен быть заполнен PRDir). Создадутся файлы *.txt со списком файлов" << std::endl;
-    std::cout << "2. Скопировать и раскидать файлы, которые в *.txt файлах(скопируется в папку с этой прогой)" << std::endl;
+    std::cout << "1. Создать список папки со списком файлов, рядом с этой прогой (в ini должен быть заполнен PRDir). " << std::endl;
+    std::cout << "2. Скопировать файлы в папки, которые прописаны в txt файле (Копировать будем в DirToCopy)" << std::endl;
 
     std::cin >> action;
 
-        if (action == 'y')
-        {   
-            CreateFolders();
-            //CreateListFiles();
-            /*while (true)
-            {
-                if (!std::filesystem::exists("PRFiles")) {
-                                           
-                    std::string path;
-                    // создаем папки с файлами
-                    path = "PRFiles";
-                    std::filesystem::create_directories(path);
-                    path = "PRFiles//Bin";
-                    std::filesystem::create_directories(path);
-                    //------------------------------------------------------------------------
-                    path = "C:\\Program Files\\Recognition Technologies\\ParkRight 3.8\\Bin";
-                    std::ofstream tempfile("temp.txt");
-                    for (auto& p : std::filesystem::directory_iterator(path))
-                    {
-                        tempfile << p << std::endl;
-                    }
-                    //------------------------------------------------------------------------
-                    tempfile.close();
-                    std::ifstream tempfileOne("temp.txt");
-                    std::ofstream binfiles("PRFiles//Bin//Bin.txt");
-                    std::string p;
-                    while (std::getline(tempfileOne, p))
-                    {
-                        p = del(p);
-                        binfiles << p << std::endl;
-                    }
-                    tempfileOne.close();
-                    std::filesystem::remove("temp.txt");
-                    binfiles.close();
-                        
-                        /*
-                    path = "PRFiles";
-                    std::filesystem::create_directories(path);
-                    path = "PRFiles";
-                    std::filesystem::create_directories(path);
-                    path = "PRFiles";
-                    std::filesystem::create_directories(path);
-                    path = "PRFiles";
-                    std::filesystem::create_directories(path);
-
-                    std::filesystem::create_directories("PRFiles//Bin");
-                    std::ofstream binfiles("PRFiles//Bin//Bin.txt");
-                    for (auto& p : std::filesystem::directory_iterator("PRFiles//Bin//Bin.txt"))
-                        std::cout << p << std::endl;
-                    std::filesystem::create_directories("PRFiles//Data");
-                    std::filesystem::create_directories("PRFiles//LocKeys");
-                    std::filesystem::create_directories("PRFiles//LocKeys//Russian");
-                    std::filesystem::create_directories("PRFiles//LocKeys//English");
-                    break;
-                }
-                else std::filesystem::remove_all("PRFiles");
-            }*/
-        }
-      
-   
-    /*
-    std::ifstream inifile("pr_check.ini");
-
-    while (true)
+    if (action == 1)
     {
-        if (inifile.is_open())
-        {
-            std::cout << "ini файл загружен успешно" << std::endl;
-            break;
-        }
-        else std::cout << "ini файл не найден" << std::endl;
-             
-
-        std::cout << "Создать ini файл? y/n: ";
-        char action;
-        std::cin >> action;
-
-        if (action == 'y')
-        {
-            std::ofstream createinifile("pr_check.ini");
-            createinifile << "ParkRightDir= " << std::endl << "CopyDir= ";
-            return 0;
-
-        }
+        //создаем папки
+        CreateFolders(FolderTxt);
+        CreateFiles(PRDir, FolderTxt);
     }
-
-    std::string dir, filedir, sourceFile;
-
-    inifile >> dir >> dir >> filedir >> filedir;
-
-
-
-    std::filesystem::copy_file(sourceFile, dir, std::filesystem::copy_options::overwrite_existing);
-
-    
-    std::string test;
-    test = "C:\\1";
-    if (!std::filesystem::exists(test)) {
-        std::filesystem::create_directories(test);
-    }*/
+    else if (action == 2)
+    {   
+        CreateFolders(DirToCopy);
+        CopyFiles(DirToCopy);
+    }
 }
 
 std::string del(std::string p)
@@ -160,25 +91,155 @@ std::string del(std::string p)
     return p;
 }
 
-std::string Parseini(std::string param, std::string t)
-{
-    if()
-    return std::string();
+std::string Parseini(std::string param, std::string temp)
+{   
+    std::string tempo = "";
+    int i;
+    for (i = 0; i < temp.length(); i++)
+    {
+        if (temp[i] != '=')
+        {
+            tempo += temp[i];
+
+        }
+        else
+        {
+            i++;
+            break;
+        }
+    }
+
+    if (param == tempo)
+    {   
+        param = "";
+        for (; i < temp.length(); i++)
+        {   
+            param += temp[i];
+        }
+    }
+
+    return param;
 }
 
-void CreateFolders()
+void CreateFolders(std::string path)
 {
     while (true)
     {
-        if (!std::filesystem::exists("PRFiles")) {
-            std::filesystem::create_directories("PRFiles");
-            std::filesystem::create_directories("PRFiles//Bin");
-            std::filesystem::create_directories("PRFiles//Data");
-            std::filesystem::create_directories("PRFiles//LocKeys");
-            std::filesystem::create_directories("PRFiles//LocKeys//Russian");
-            std::filesystem::create_directories("PRFiles//LocKeys//English");
+        if (!std::filesystem::exists(path)) {
+            std::filesystem::create_directories(path);
+            std::filesystem::create_directories(path + "//Bin");
+            std::filesystem::create_directories(path + "//Data");
+            std::filesystem::create_directories(path + "//LocKeys");
+            std::filesystem::create_directories(path + "//LocKeys//Russian");
+            std::filesystem::create_directories(path + "//LocKeys//English");
             break;
         }
-        else std::filesystem::remove_all("PRFiles");
+        else std::filesystem::remove_all(path);
     }
+}
+
+void CreateFiles(std::string path,std::string PathToMakeTxt)
+{
+
+    std::string temp;
+    std::string RemovePathTemp;
+    
+   
+
+    //создаем для Bin
+
+    std::ofstream binTemp("D://BinTemp.txt");
+    std::ofstream bin(PathToMakeTxt + "//Bin//Bin.txt");
+
+    for (auto& p : std::filesystem::directory_iterator(path + "\\Bin"))
+    {
+        binTemp << p << std::endl;
+    }
+
+    binTemp.close();
+    std::ifstream ReadBinTemp("D://BinTemp.txt");
+
+    while (std::getline(ReadBinTemp, temp))
+    {
+        temp = del(temp);
+        bin << temp << std::endl;
+    }
+
+    ReadBinTemp.close();
+
+    //создаем для Data
+
+    std::ofstream DataTemp("D://DataTemp.txt");
+    std::ofstream Data(PathToMakeTxt + "//Data//Data.txt");
+
+    for (auto& p : std::filesystem::directory_iterator(path + "\\Data"))
+    {
+        DataTemp << p << std::endl;
+    }
+
+    DataTemp.close();
+
+    std::ifstream ReadDataTemp("D://DataTemp.txt");
+
+    while (std::getline(ReadDataTemp, temp))
+    {
+        temp = del(temp);
+        Data << temp << std::endl;
+    }
+
+    ReadDataTemp.close();
+
+    //Russian Local
+
+    std::ofstream RussianTemp("D://RussianTemp.txt");
+    std::ofstream Russian(PathToMakeTxt + "//LocKeys//Russian//Russian.txt");
+
+    for (auto& p : std::filesystem::directory_iterator(path + "\\LocKeys\\Russian"))
+    {
+        RussianTemp << p << std::endl;
+    }
+
+    RussianTemp.close();
+    std::ifstream ReadRussianTemp("D://RussianTemp.txt");
+
+    while (std::getline(ReadRussianTemp, temp))
+    {
+        temp = del(temp);
+        Russian << temp << std::endl;
+    }
+
+    ReadRussianTemp.close();
+
+
+    //English Local
+
+    std::ofstream EnglishTemp("D://EnglishTemp.txt");
+    std::ofstream English(PathToMakeTxt + "//LocKeys//English//English.txt");
+
+    for (auto& p : std::filesystem::directory_iterator(path + "\\LocKeys\\English"))
+    {
+        EnglishTemp << p << std::endl;
+    }
+
+    EnglishTemp.close();
+    std::ifstream ReadEnglishTemp("D://EnglishTemp.txt");
+
+    while (std::getline(ReadEnglishTemp, temp))
+    {
+        temp = del(temp);
+        English << temp << std::endl;
+    }
+
+    ReadEnglishTemp.close();
+
+    remove("D://BinTemp.txt");
+    remove("D://DataTemp.txt");
+    remove("D://RussianTemp.txt");
+    remove("D://EnglishTemp.txt");
+
+}
+
+void CopyFiles(std::string path)
+{
+    std::filesystem::copy_file
 }
